7c8a009d11467effeb1ad5b6128eadfe0f9d50f1
omithehacker <omoladeaboaba12@gmail.com>
2024-12-16 16:28:24 +0100

    uhh yeah yeah

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Contact() {
   return (
     <div id="contact">
-      <h2>Contact us!</h2>
+      <h2>Contact us now!!!</h2>
       <p>Phone: +2347047762237</p>
       <p>Email: <a href="mailto:omoladeaboaba12@gmail.com">omoladeaboaba12@gmail.com</a></p>
       <p>Address: A road like that.</p>
